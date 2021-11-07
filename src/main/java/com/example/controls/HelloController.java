fccffa447464732bc9a45e8a200e004480594df8
Prince kunzwa <princekunzwa@outlook.com>
2021-11-07 18:56:30 +0000

    Events and threads initial push.

--- src/main/java/com/example/controls/HelloController.java
+++ src/main/java/com/example/controls/HelloController.java
@@ -1,14 +1,69 @@
 package com.example.controls;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField nameField;
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
+    @FXML
+    private CheckBox ourCheckBox;
+
+    @FXML
+    public void onButtonClicked(ActionEvent e){
+
+        if(e.getSource().equals(helloButton)){
+            System.out.println("Hello, " + nameField.getText());
+        } else if(e.getSource().equals(byeButton)){
+            System.out.println("Bye, " + nameField.getText());
+        }
+
+//        try{
+//            thread.sleep(10000);
+//        }catch (InterruptedException event){
+//
+//        }
+        if (ourCheckBox.isSelected()){
+            nameField.clear();
+            helloButton.setDisable(true);
+            byeButton.setDisable(true);
+        }
     }
+
+    @FXML
+    public void initialize(){
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
+
+    @FXML
+    public void handleKeyReleased() {
+        String text = nameField.getText();
+        boolean disableButtons = text.isEmpty() | text.trim().isEmpty();
+        helloButton.setDisable(disableButtons);
+        byeButton.setDisable(disableButtons);
+    }
+
+    @FXML
+    public void handleChange(){
+        System.out.println("The checkbox is " + (ourCheckBox.isSelected() ? "checked" : "not checked"));
+    }
+
+
+//    @FXML
+//    private Label welcomeText;
+//
+//    @FXML
+//    protected void onHelloButtonClick() {
+//        welcomeText.setText("Welcome to JavaFX Application!");
+//    }
 }
